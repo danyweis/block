8fa6c5024c3910af6fdf22db7df383081f07c820
danyweis <danyweismail@gmail.com>
2022-01-23 00:21:56 +0700

    Custome Iterators part 2

--- custome_iterators.rb
+++ custome_iterators.rb
@@ -17,9 +17,29 @@ movie2 = Movie.new("Cast Away", "PG", 143)
 movie3 = Movie.new("Apollo 13", "PG", 140)
 movie4 = Movie.new("Cars", "G", 117)
 
+module MyEnumerable
+  def my_select
+    new_arr = []
+    each do |value|
+      new_arr << value if yield(value)
+    end
+    new_arr
+  end
+
+  def my_map
+    new_arr = []
+    each do |value|
+      new_arr << yield(value)
+    end
+    new_arr
+  end
+end
+
 class MovieQueue
 
-  include Enumerable #to be able to call select on a class object 
+
+  include MyEnumerable #to be able to call select on a class object 
+  # include Enumerable #to be able to call select on a class object 
   # the Enumerable requiers as well an EACH method to be defined in the class
 
   def initialize(name)
@@ -48,5 +68,15 @@ queue.add_movie(movie3)
 queue.each { |movie| movie.watch }
 queue.each_by_rating("PG") { |movie| movie.watch } 
 
-long_movies = queue.select { |m| m.duration > 100 }
+# long_movies = queue.select { |m| m.duration > 100 }
+long_movies = queue.my_select { |m| m.duration > 100 }
+puts "long"
 p long_movies
+
+puts "MAP"
+
+titles = queue.my_map { |m| m.title.downcase }
+p titles
+
+puts "title with 13"
+# p queue.detect { |m| m.title =~ /13/ }
--- playlist.rb
+++ playlist.rb
@@ -31,9 +31,61 @@ song2 = Song.new("Stan", "Eminem", 2.56)
 song3 = Song.new("Run the world", "Beyonce", 5.03)
 song4 = Song.new("LaLisa", "Lisa", 3.29)
 
+module MyEnumerable
+  def my_select
+    new_arr = []
+    each do |value|
+      new_arr << value if yield(value)
+    end
+    new_arr
+  end
+
+  def my_map
+    new_arr = []
+    each do |value|
+      new_arr << yield(value)
+    end
+    new_arr
+  end
+
+  def my_reject
+    new_arr = []
+    each do |value|
+      new_arr << value unless yield(value)
+    end
+    new_arr
+  end
+
+  def my_detect
+    # new_arr = []
+    each do |value|
+      return value if yield(value)
+    end
+    nil
+  end 
+
+  def my_any?
+    # new_arr = []
+    each do |value|
+      return true if yield(value)
+    end
+  false
+  end
+
+  def my_reduce(num=0)
+    val = num
+    each do |value|
+      val = yield(val, value)
+    end
+    val
+  end
+
+end
+
 class Playlist
 
-include Enumerable
+  include MyEnumerable
+# include Enumerable
 
   def initialize(name)
     @name = name
@@ -73,7 +125,7 @@ playlist.each { |s| s.play }
 puts
 playlist.play_song
 puts
-over3min = playlist.select { |song| song.duration > 3.0}
+over3min = playlist.my_select { |song| song.duration > 3.0}
 puts over3min
 puts
 playlist.each_tagline { |tagline| puts tagline }
@@ -81,3 +133,18 @@ puts
 playlist.each_by_artist("JayZ") { |song| song.play }
 puts
 song3.each_file_name { |filenames| puts filenames}
+
+puts
+song_labels = playlist.my_map { |song| "#{song.name} => #{song.artist}" }
+puts song_labels
+
+puts 
+no_paris_song = playlist.my_reject { |song| song.name =~ /Paris/}
+p no_paris_song
+
+p playlist.my_detect { |song| song.artist == "Eminem"}
+p playlist.my_any? { |song| song.artist == "Eminem"}
+
+puts "reduce:"
+total_duration = playlist.my_reduce(0) { |sum, song| sum + song.duration }
+p total_duration
